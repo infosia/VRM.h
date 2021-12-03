d3e51fd1eeeaa963afc0c58ed2f3359279d80da0
Kota Iguchi <developer@infosia.co.jp>
2021-12-03 15:06:34 +0900

    Add 1.0 read test and models

--- tests/src/unit-json.cpp
+++ tests/src/unit-json.cpp
@@ -36,3 +36,34 @@ TEST_CASE("VRM 0.x") {
             VRMC_VRM_0_0::Meta::AllowedUserName::OnlyAuthor);
   }
 }
+
+TEST_CASE("VRM 1.0") {
+  std::filesystem::path f{"../data/1.0/001.vrm"};
+  const auto doc = fx::gltf::LoadFromBinary(f);
+
+  SECTION("load-001-extensions") {
+    REQUIRE(doc.extensionsUsed.size() > 0);
+    REQUIRE(doc.extensionsAndExtras.size() > 0);
+    REQUIRE(doc.extensionsAndExtras.contains("extensions"));
+
+    const auto extensions = doc.extensionsAndExtras["extensions"];
+    REQUIRE(extensions.contains("VRMC_vrm"));
+    REQUIRE(extensions["VRMC_vrm"].size() > 0);
+
+    VRMC_VRM_1_0::Vrm vrm;
+    VRMC_VRM_1_0::from_json(extensions["VRMC_vrm"], vrm);
+
+    REQUIRE_FALSE(vrm.specVersion.empty());
+  }
+
+  SECTION("load-001.meta") {
+      const auto extensions = doc.extensionsAndExtras["extensions"];
+
+      VRMC_VRM_1_0::Vrm vrm;
+      VRMC_VRM_1_0::from_json(extensions["VRMC_vrm"], vrm);
+
+      REQUIRE_FALSE(vrm.meta.version.empty());
+      REQUIRE(vrm.meta.avatarPermission == VRMC_VRM_1_0::Meta::AvatarPermissionType::OnlyAuthor);
+  }
+
+}
