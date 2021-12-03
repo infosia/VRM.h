93a980b931141679e45c374291b6c04b60eec81d
Kota Iguchi <developer@infosia.co.jp>
2021-12-03 14:13:36 +0900

    Add vrm 0.x read test

---
+++ tests/src/unit-json.cpp
@@ -0,0 +1,38 @@
+#include <catch2/catch.hpp>
+#include <filesystem>
+#include <iostream>
+
+#define USE_VRMC_VRM_0_0
+#define USE_VRMC_VRM_1_0
+#include <VRMC/VRM.h>
+
+TEST_CASE("VRM 0.x") {
+  std::filesystem::path f{"../data/0.x/001.vrm"};
+  const auto doc = fx::gltf::LoadFromBinary(f);
+
+  SECTION("load-001-extensions") {
+    REQUIRE(doc.extensionsUsed.size() > 0);
+    REQUIRE(doc.extensionsAndExtras.size() > 0);
+    REQUIRE(doc.extensionsAndExtras.contains("extensions"));
+
+    const auto extensions = doc.extensionsAndExtras["extensions"];
+    REQUIRE(extensions.contains("VRM"));
+    REQUIRE(extensions["VRM"].size() > 0);
+
+    VRMC_VRM_0_0::Vrm vrm;
+    VRMC_VRM_0_0::from_json(extensions["VRM"], vrm);
+
+    REQUIRE_FALSE(vrm.specVersion.empty());
+  }
+
+  SECTION("load-001.meta") {
+    const auto extensions = doc.extensionsAndExtras["extensions"];
+
+    VRMC_VRM_0_0::Vrm vrm;
+    VRMC_VRM_0_0::from_json(extensions["VRM"], vrm);
+
+    REQUIRE_FALSE(vrm.meta.version.empty());
+    REQUIRE(vrm.meta.allowedUserName ==
+            VRMC_VRM_0_0::Meta::AllowedUserName::OnlyAuthor);
+  }
+}
