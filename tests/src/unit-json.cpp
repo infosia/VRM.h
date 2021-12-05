29a3cef302b9757f57d6b64b458f68814ccde2fc
Kota Iguchi <developer@infosia.co.jp>
2021-12-05 17:35:09 +0900

    clang-format

--- tests/src/unit-json.cpp
+++ tests/src/unit-json.cpp
@@ -60,100 +60,101 @@ TEST_CASE("VRM 1.0 (fx-gltf)") {
   }
 
   SECTION("load-001.meta") {
-      const auto extensions = doc.extensionsAndExtras["extensions"];
+    const auto extensions = doc.extensionsAndExtras["extensions"];
 
-      VRMC_VRM_1_0::Vrm vrm;
-      VRMC_VRM_1_0::from_json(extensions["VRMC_vrm"], vrm);
+    VRMC_VRM_1_0::Vrm vrm;
+    VRMC_VRM_1_0::from_json(extensions["VRMC_vrm"], vrm);
 
-      REQUIRE_FALSE(vrm.meta.version.empty());
-      REQUIRE(vrm.meta.avatarPermission == VRMC_VRM_1_0::Meta::AvatarPermissionType::OnlyAuthor);
+    REQUIRE_FALSE(vrm.meta.version.empty());
+    REQUIRE(vrm.meta.avatarPermission ==
+            VRMC_VRM_1_0::Meta::AvatarPermissionType::OnlyAuthor);
   }
-
 }
 
 TEST_CASE("VRM 0.x (cgltf)") {
-    cgltf_options options = {};
-    cgltf_data* data = NULL;
-    cgltf_result result = cgltf_parse_file(&options, "../data/0.x/001.vrm", &data);
+  cgltf_options options = {};
+  cgltf_data *data = NULL;
+  cgltf_result result =
+      cgltf_parse_file(&options, "../data/0.x/001.vrm", &data);
 
-    REQUIRE(result == cgltf_result_success);
-    REQUIRE(data->data_extensions_count > 0);
+  REQUIRE(result == cgltf_result_success);
+  REQUIRE(data->data_extensions_count > 0);
 
-    char* vrm_json = nullptr;
-    for (cgltf_size i = 0; i < data->data_extensions_count; ++i) {
-        if (strcmp(data->data_extensions[i].name, "VRM") == 0) {
-            vrm_json = data->data_extensions[i].data;
-            break;
-        }
+  char *vrm_json = nullptr;
+  for (cgltf_size i = 0; i < data->data_extensions_count; ++i) {
+    if (strcmp(data->data_extensions[i].name, "VRM") == 0) {
+      vrm_json = data->data_extensions[i].data;
+      break;
     }
-    REQUIRE(vrm_json != nullptr);
+  }
+  REQUIRE(vrm_json != nullptr);
 
-    const nlohmann::json VRM_OBJ = nlohmann::json::parse(vrm_json);
+  const nlohmann::json VRM_OBJ = nlohmann::json::parse(vrm_json);
 
-    SECTION("load-001-extensions") {
-        REQUIRE(VRM_OBJ.is_object());
+  SECTION("load-001-extensions") {
+    REQUIRE(VRM_OBJ.is_object());
 
-        VRMC_VRM_0_0::Vrm vrm;
-        VRMC_VRM_0_0::from_json(VRM_OBJ, vrm);
+    VRMC_VRM_0_0::Vrm vrm;
+    VRMC_VRM_0_0::from_json(VRM_OBJ, vrm);
 
-        REQUIRE_FALSE(vrm.specVersion.empty());
-    }
+    REQUIRE_FALSE(vrm.specVersion.empty());
+  }
 
-    SECTION("load-001.meta") {
-        REQUIRE(VRM_OBJ.is_object());
-        REQUIRE(VRM_OBJ.contains("meta"));
+  SECTION("load-001.meta") {
+    REQUIRE(VRM_OBJ.is_object());
+    REQUIRE(VRM_OBJ.contains("meta"));
 
-        VRMC_VRM_0_0::Vrm vrm;
-        VRMC_VRM_0_0::from_json(VRM_OBJ, vrm);
+    VRMC_VRM_0_0::Vrm vrm;
+    VRMC_VRM_0_0::from_json(VRM_OBJ, vrm);
 
-        REQUIRE_FALSE(vrm.meta.version.empty());
-        REQUIRE(vrm.meta.allowedUserName ==
+    REQUIRE_FALSE(vrm.meta.version.empty());
+    REQUIRE(vrm.meta.allowedUserName ==
             VRMC_VRM_0_0::Meta::AllowedUserName::OnlyAuthor);
-    }
+  }
 
-    if (result == cgltf_result_success)
-    {
-        cgltf_free(data);
-    }
+  if (result == cgltf_result_success) {
+    cgltf_free(data);
+  }
 }
 
 TEST_CASE("VRM 1.0 (cgltf)") {
-    cgltf_options options = {};
-    cgltf_data* data = NULL;
-    cgltf_result result = cgltf_parse_file(&options, "../data/1.0/001.vrm", &data);
+  cgltf_options options = {};
+  cgltf_data *data = NULL;
+  cgltf_result result =
+      cgltf_parse_file(&options, "../data/1.0/001.vrm", &data);
 
-    REQUIRE(result == cgltf_result_success);
-    REQUIRE(data->data_extensions_count > 0);
+  REQUIRE(result == cgltf_result_success);
+  REQUIRE(data->data_extensions_count > 0);
 
-    char* vrm_json = nullptr;
-    for (cgltf_size i = 0; i < data->data_extensions_count; ++i) {
-        if (strcmp(data->data_extensions[i].name, "VRMC_vrm") == 0) {
-            vrm_json = data->data_extensions[i].data;
-            break;
-        }
+  char *vrm_json = nullptr;
+  for (cgltf_size i = 0; i < data->data_extensions_count; ++i) {
+    if (strcmp(data->data_extensions[i].name, "VRMC_vrm") == 0) {
+      vrm_json = data->data_extensions[i].data;
+      break;
     }
-    REQUIRE(vrm_json != nullptr);
+  }
+  REQUIRE(vrm_json != nullptr);
 
-    const nlohmann::json VRM_OBJ = nlohmann::json::parse(vrm_json);
+  const nlohmann::json VRM_OBJ = nlohmann::json::parse(vrm_json);
 
-    SECTION("load-001-extensions") {
-        REQUIRE(VRM_OBJ.is_object());
+  SECTION("load-001-extensions") {
+    REQUIRE(VRM_OBJ.is_object());
 
-        VRMC_VRM_1_0::Vrm vrm;
-        VRMC_VRM_1_0::from_json(VRM_OBJ, vrm);
+    VRMC_VRM_1_0::Vrm vrm;
+    VRMC_VRM_1_0::from_json(VRM_OBJ, vrm);
 
-        REQUIRE_FALSE(vrm.specVersion.empty());
-    }
+    REQUIRE_FALSE(vrm.specVersion.empty());
+  }
 
-    SECTION("load-001.meta") {
-        REQUIRE(VRM_OBJ.is_object());
-        REQUIRE(VRM_OBJ.contains("meta"));
+  SECTION("load-001.meta") {
+    REQUIRE(VRM_OBJ.is_object());
+    REQUIRE(VRM_OBJ.contains("meta"));
 
-        VRMC_VRM_1_0::Vrm vrm;
-        VRMC_VRM_1_0::from_json(VRM_OBJ, vrm);
+    VRMC_VRM_1_0::Vrm vrm;
+    VRMC_VRM_1_0::from_json(VRM_OBJ, vrm);
 
-        REQUIRE_FALSE(vrm.meta.version.empty());
-        REQUIRE(vrm.meta.avatarPermission == VRMC_VRM_1_0::Meta::AvatarPermissionType::OnlyAuthor);
-    }
-
+    REQUIRE_FALSE(vrm.meta.version.empty());
+    REQUIRE(vrm.meta.avatarPermission ==
+            VRMC_VRM_1_0::Meta::AvatarPermissionType::OnlyAuthor);
+  }
 }
